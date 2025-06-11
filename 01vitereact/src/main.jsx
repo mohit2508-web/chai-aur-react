482d0a7112b4bafbeda31be82bd234402fcbc5fd
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-06-11 11:50:04 +0530

    custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const reactElement=React.createElement(
+  'a',
+  {href:'https://google.com/github/hiteshchaudhary',target: '_blank'},
+  'click me to visit github'
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+reactElement
 )
